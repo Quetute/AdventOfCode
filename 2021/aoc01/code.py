04d9e76b1e496193e1abdb6fa9922970dc7874c6
Antoine Queru <antoine.queru@dataiku.com>
2021-12-01 14:36:56 +0100

    2021 day 01 cleaner ?

--- 2021/aoc01/code.py
+++ 2021/aoc01/code.py
@@ -2,18 +2,10 @@ with open('input.txt', 'r') as reader:
    depths = [int(line) for line in reader.readlines()]
 
 def part1():
-    count = 0
-    for i in range(1, len(depths)):
-        if depths[i-1] < depths[i]:
-            count += 1
-    return count
+    return sum(1 if depths[i-1] < depths[i] else 0 for i in range(1, len(depths)))
 
 def part2():
-    count = 0
-    for i in range(1, len(depths) - 2):
-        if depths[i-1] < depths[i+2]:
-            count += 1
-    return count
+    return sum(1 if depths[i -1] < depths[i+2] else 0 for i in range(1, len(depths) - 2))
 
 print(part1())
 print(part2())
